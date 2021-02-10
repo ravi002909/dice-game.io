4ae057cf9402326f4a8782789c0d8b4d515a8eb5
ravi002909 <66548045+ravi002909@users.noreply.github.com>
2021-02-10 13:32:42 +0530

    Create java.js

---
+++ java.js
@@ -0,0 +1,22 @@
+var randomnumber1=Math.round(Math.random()*5+1);
+var p="/Users/ravindrameena/Downloads/Dicee Challenge - Starting Files/images/dice" + randomnumber1 +  ".png"
+var d=document.querySelectorAll("img")[0];
+d.setAttribute("src",p);
+
+var randomnumber2=Math.round(Math.random()*5+1);
+var ef="/Users/ravindrameena/Downloads/Dicee Challenge - Starting Files/images/dice" + randomnumber2 +  ".png"
+var r=document.querySelectorAll("img")[1];
+r.setAttribute("src",ef);
+
+if(randomnumber1 > randomnumber2)
+{
+    document.querySelector("h1").innerHTML="🚩 Player 1 wins";
+}
+else if(randomnumber2> randomnumber1)
+{
+    document.querySelector("h1").innerHTML="🚩 Player 2 wins";
+}
+else
+{
+    document.querySelector("h1").innerHTML="🚩 Draw";
+}
